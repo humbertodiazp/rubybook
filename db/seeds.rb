bbab615c2c70f71eaa035354e643467023b3dfbb
Humberto <hdkaratechop@gmail.com>
2023-05-17 22:20:44 -0700

    seed database with test users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'hello@tester.com', password: 'test123', username: 'hellotester')
+User.create(email: 'goodby@tester.com', password: 'test123', username: 'goodbyetester')
