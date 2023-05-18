ed1cab8eaf0f4e26e595249d4e0edcd99eb6db6e
Humberto <hdkaratechop@gmail.com>
2023-05-17 22:15:13 -0700

    add username attribute the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
    
     def configure_permitted_parameters
-        added_attrs = [:email, :password, :password_confirmation, :remember_me]
+        added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
         devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
         devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
         devise_parameter_sanitizer.permit :account_update, keys: added_attrs
