6d2a824ffe009d72fcfacc39fb7f10946c6bf41c
Humberto <hdkaratechop@gmail.com>
2023-05-23 22:43:02 -0700

    Add methods/params for index, new, & create

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,9 +1,19 @@
 class FriendRequestsController < ApplicationController
-    def index
-      @friend_requests = FriendRequest.all
-    end
+  def index
+    @friend_requests = FriendRequest.all
+  end
   
-    def new
-      @friend_request = FriendRequest.new
+  def new
+    @friend_request = FriendRequest.new
+  end
+
+  def create 
+    @friend_request = FriendRequest.new(friend_request_params)
+  end
+
+  private 
+    # Only allow a list of trusted parameters through.
+    def friend_request_params 
+      params.require(:friend_request).permit(:requester_id, :receiver_id)
     end
- 
+end
