3070f872c23a3a20e154c7fd3338b90d24bc7b25
Humberto <hdkaratechop@gmail.com>
2023-05-23 22:42:09 -0700

    Add logic to accpt frnd reqs, and add/rmv friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,31 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :user_id
   has_many :friendships
   has_many :friends, through: :friendships
-  has_many :requested_friend_requests, foreign_key: :requester_id, class_name: "Friendship", dependent: :destroy
+  has_many :sent_friend_requests, foreign_key: :requester_id, class_name: "Friendship", dependent: :destroy
   has_many :received_friend_requests, foreign_key: :receiver_id, class_name: "Friendship", dependent: :destroy
 
 
   validates :email, uniqueness: true
   validates :username, uniqueness: true
+
+  def add_friend(user)
+    friends << user unless friends.include?(user)
+  end
+
+  def accept_friend_request
+    friend_request = FriendRequest.find(params[:friend_request_id])
+    user = friend_request.requestor
+    current_user.add_friend(user)
+    friend_request.destroy
+    # Handle the response or redirect as needed
+  end
+  
+  def remove_friend(user)
+    friends.destroy(user)
+  end
+
+
+
+
+
 end
