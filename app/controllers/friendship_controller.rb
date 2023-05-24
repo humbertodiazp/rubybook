5bcb84b168f26aa7215b2500e1ab8d59faf3533f
Humberto <hdkaratechop@gmail.com>
2023-05-23 22:50:39 -0700

    Add the create method to handle friendships

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,2 +1,14 @@
 class FriendshipController < ApplicationController
+  def index
+    @friendships = Friendship.all
+  end
+  
+  def new 
+    @friendship = Friendship.new
+  end
+
+  def create
+    friend_request = FriendRequest.find(params[:friend_request_id])
+  end
 end
+
