7c7542f9705ad9b942d4c89f4e865343fc807e62
Humberto <hdkaratechop@gmail.com>
2023-05-24 23:32:53 -0700

    add create method and define friendship parameters

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,14 +1,27 @@
 class FriendshipController < ApplicationController
-  def index
-    @friendships = Friendship.all
-  end
-  
-  def new 
-    @friendship = Friendship.new
-  end
+    
+    def index
+        @friendships = Friendship.all
+    end
+    # GET /friendships/new
+    def new 
+        @friendship = Friendship.new
+    end
 
-  def create
-    friend_request = FriendRequest.find(params[:friend_request_id])
-  end
+    # POST /friendships or /friendships.json
+    def create
+        @friendship = Friendship.find(params[:friendship_params])
+        return unless @friend_request.save
+
+        redirect_to request.referrer
+    end
+    
+    
+    private
+    # Only allow a list of trusted parameters through.
+    def friendship_params
+        params.require(:friendship).permit(user_id, friend_id)
+    end
 end
-
+    
+    
